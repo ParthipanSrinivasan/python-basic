d9aa74bc924af04cb844d903d57d909b592467cc
ParthipanSrinivasan <parthipanks2001@gmail.com>
2026-01-27 12:15:22 +0530

    Data Type added

--- basic.py
+++ basic.py
@@ -1,7 +1,31 @@
-x = True;
-y = 7;
-z = "Developer";
-a = ["aplle","bananna","orange"];
-print(type (a));
-print(type (z));
-print(a);
+x = 'Parthipan'; #str String
+y = 20; #int Integer
+z = 20.5; #float Float
+a = 1j  #complex
+b = ['aplle', 'banna', 'cherry'] #list
+c = ('apple', 'banana', 'cherry') #tuple
+d = range(6) #range
+e = {"name": "john", "age": 36} #dict
+f = {"apple", "banana", "cherry"} #set
+g = frozenset({"aplle", "banana", "cherry"}) #frozenset
+h = True #boolean
+i = b"Hello" #bytes
+j = bytearray(5) #bytearray
+k = memoryview(bytes(5)) #memoryview
+l = None #noneType
+ 
+#Setting the Specific Data Type
+a1 = str('parthipan');
+a2 = int(20) 
+a3 = float(20.5)
+a4 = complex(1j)
+a5 = list(("aplle", "banana", "cherry"));
+a6 = tuple(("apple", "banana", "cherry"));
+a7 = range(6)
+a8 = dict(name="John", age=36)
+a9 = set(("apple", "banana", "cherry"))
+a10 = frozenset(("aplle", "banana", "cherry"))
+a11 = bool(5)
+a12 = bytes(5)
+a13 = bytearray(5)
+a14 = memoryview(bytes(5))
