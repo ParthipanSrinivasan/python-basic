7bb95665a6bd0f0217c9f0304d933d3dd3de9bdc
ParthipanSrinivasan <parthipanks2001@gmail.com>
2026-02-02 12:10:01 +0530

    Learn Module code

---
+++ module.py
@@ -0,0 +1,6 @@
+import myModule
+
+print(myModule.person['age'])
+print(myModule.fruits)
+for fruitsname in myModule.fruits:
+    print('They Fruit Name IS'+" "+fruitsname)
---
+++ myModule.py
@@ -0,0 +1,8 @@
+person = {
+    "name" : "Parthipan",
+    "age" : 24,
+    "place" : "Dharmapuri",
+    "address" : "4/783 MGR-Nager"
+}
+
+fruits = ["apple", "banana", "cherry"]
